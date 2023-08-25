7a98f8cb200d4c50a2047729af50a4548fcbe399
Hero Saai <Herosaai@hotmail.com>
2023-08-25 06:21:01 +0200

    switching to EntityFrameworkCore sql

---
+++ Models/BookStoreDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BookStore.Models
+{
+    public class BookStoreDbContext : DbContext
+    {
+        public BookStoreDbContext(DbContextOptions<BookStoreDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Author> Authors { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,50 +1,54 @@
 using BookStore.Models;
 using BookStore.Models.Reopsitories;
+using Microsoft.EntityFrameworkCore;
 
 namespace BookStore
 {
-	public class Program
-	{
-		public static void Main(string[] args)
-		{
-			var builder = WebApplication.CreateBuilder(args);
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var Configuration = builder.Configuration;
 
-			// Add services to the container.
-			builder.Services.AddControllersWithViews().AddViewOptions(options =>
-			{
-				options.HtmlHelperOptions.ClientValidationEnabled = true;
-			});
-			// Combined service for controllers and views
+            // Add services to the container.
+            builder.Services.AddControllersWithViews().AddViewOptions(options =>
+            {
+                options.HtmlHelperOptions.ClientValidationEnabled = true;
+            });
 
-			// Add the memory cache service
-			builder.Services.AddSingleton<IBookStoreRepository<Author>, AuthorRepository>();
-			builder.Services.AddSingleton<IBookStoreRepository<Book>, BookRepository>();
+            // the  services
+            builder.Services.AddSingleton<IBookStoreRepository<Author>, AuthorRepository>();
+            builder.Services.AddSingleton<IBookStoreRepository<Book>, BookRepository>();
+            builder.Services.AddDbContext<BookStoreDbContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("SqlCon"));
+            });
+            var app = builder.Build();
 
-			var app = builder.Build();
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
 
-			// Configure the HTTP request pipeline.
-			if (!app.Environment.IsDevelopment())
-			{
-				app.UseExceptionHandler("/Error");
-				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-				app.UseHsts();
-			}
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
 
-			app.UseHttpsRedirection();
-			app.UseStaticFiles();
+            app.UseRouting();
 
-			app.UseRouting();
+            app.UseAuthorization();
 
-			app.UseAuthorization();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Author}/{action=Index}/{id?}");
+            });
 
-			app.UseEndpoints(endpoints =>
-			{
-				endpoints.MapControllerRoute(
-					name: "default",
-					pattern: "{controller=Author}/{action=Index}/{id?}");
-			});
-
-			app.Run();
-		}
-	}
+            app.Run();
+        }
+    }
 }
