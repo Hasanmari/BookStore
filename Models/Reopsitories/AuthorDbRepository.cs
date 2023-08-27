9abe3ebb71e0a8f455bbe05f4054d33fb662458a
Hero Saai <Herosaai@hotmail.com>
2023-08-27 13:10:58 +0200

    made the new DB repository

---
+++ Models/Reopsitories/AuthorDbRepository.cs
@@ -0,0 +1,42 @@
+﻿namespace BookStore.Models.Reopsitories
+{
+    public class AuthorDbRepository
+    {
+        private readonly BookStoreDbContext DB;
+
+        public AuthorDbRepository(BookStoreDbContext _bookStoreDbContext)
+        {
+            DB = _bookStoreDbContext;
+        }
+
+        public void Add(Author author)
+        {
+            DB.Authors.Add(author);
+            DB.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            var author = Find(id);
+            DB.Authors.Remove(author);
+            DB.SaveChanges();
+        }
+
+        public Author Find(int id)
+        {
+            var author = DB.Authors.SingleOrDefault(a => a.Id == id);
+            return author;
+        }
+
+        public IList<Author> List()
+        {
+            return DB.Authors.ToList();
+        }
+
+        public void Update(int id, Author newAuthor)
+        {
+            DB.Update(newAuthor);
+            DB.SaveChanges();
+        }
+    }
+}
---
+++ Models/Reopsitories/BookDbRepository.cs
@@ -0,0 +1,42 @@
+﻿namespace BookStore.Models.Reopsitories
+{
+    public class BookDbRepository
+    {
+        private readonly BookStoreDbContext DB;
+
+        public BookDbRepository(BookStoreDbContext _bookStoreDbContext)
+        {
+            DB = _bookStoreDbContext;
+        }
+
+        public void Add(Book book)
+        {
+            DB.Books.Add(book);
+            DB.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            var book = Find(id);
+            DB.Books.Remove(book);
+            DB.SaveChanges();
+        }
+
+        public Book Find(int id)
+        {
+            var book = DB.Books.SingleOrDefault(b => b.Id == id);
+            return book;
+        }
+
+        public IList<Book> List()
+        {
+            return DB.Books.ToList();
+        }
+
+        public void Update(int id, Book newBook)
+        {
+            DB.Update(newBook);
+            DB.SaveChanges();
+        }
+    }
+}
